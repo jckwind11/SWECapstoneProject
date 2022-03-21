28483d11a79f6311da376c0b8b30157ab89ee559
Jack Windham <jckwind11@vt.edu>
2022-03-21 14:56:01 -0400

    added landing page, and a color-scheme.txt

--- college-finder/src/App.js
+++ college-finder/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <i class="material-icons school">school</i>
+        <br/><br/>
+        <h1>Welcome to College Finder</h1>
+        <button class="Sign-Up-Button">Sign Up</button>
+        <h2>or</h2>
+        <span> Already have an account? <button class="Login-Button">Login</button></span>
       </header>
     </div>
   );
