261ff4ffceb6a23d6ea5528289ba59bb5c6ae54e
Jack Windham <jckwind11@vt.edu>
2022-04-03 23:21:00 -0400

    fixed issue

--- college-finder-app/src/app/app.module.ts
+++ college-finder-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashComponent,
-    HomeComponent
+    HomeComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
--- college-finder-app/src/app/splash/splash.component.ts
+++ college-finder-app/src/app/splash/splash.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: 'app-splash',
-  templateUrl: 'splash.component.html',
-  styleUrls: ['splash.component.css']
+  selector: 'app-splash',
+  templateUrl: './splash.component.html',
+  styleUrls: ['./splash.component.css']
 })
 export class SplashComponent implements OnInit {
 
