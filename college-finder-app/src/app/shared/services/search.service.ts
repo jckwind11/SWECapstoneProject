fa77d7f1002cc57544c852987099216c09404506
Jack Windham <jckwind11@vt.edu>
2022-04-25 17:59:13 -0400

    started on searching api

--- college-finder-app/src/app/shared/services/search.service.ts
+++ college-finder-app/src/app/shared/services/search.service.ts
@@ -13,11 +13,30 @@ export class SearchService {
   constructor(private http: HttpClient) { }
 
   getAll() {
+    const params = { "api_key": this.apiKey };
+
     return this.http.get<SearchResults>(`http://localhost:3030/parecord/getparecords`);
   }
 
 
-  linkFactory() {
-
+  linkFactory(queryParams: any, fields: string[]) {
+    var link = `https://api.data.gov/ed/collegescorecard/v1/schools`;
+    link += this.addQueryParams(queryParams);
+    link += this.addFields(fields);
+    console.log(link);
+    return link;
   }
+
+  addQueryParams(queryParams: any): string {
+    const keys = []
+    for (const key in queryParams) {
+      keys.push(encodeURIComponent(key) + "=" + encodeURIComponent(queryParams[key]));
+    }
+    return keys.join('&');
+  }
+
+  addFields(fields: string[]) {
+    return fields.join(',');
+  }
+
 }
