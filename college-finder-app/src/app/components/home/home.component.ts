904feea3d6b8588bf90c32e81c150404b28e2787
Matthew Lyman <mjlyman@comcast.net>
2022-05-02 16:42:57 -0400

    Started work on recommendations

--- college-finder-app/src/app/components/home/home.component.ts
+++ college-finder-app/src/app/components/home/home.component.ts
@@ -5,6 +5,11 @@ import { SchoolSearchResults } from '../../shared/models/search/SchoolSearchResu
 import { FavoritesService } from 'src/app/shared/services/favorites.service';
 import { Observable } from 'rxjs';
 import { UserFavorites } from 'src/app/shared/models/favorite/favorite';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { SearchResults } from 'src/app/shared/models/search/SearchResults';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { SurveyForm } from 'src/app/shared/models/survey/survey';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +18,7 @@ import { UserFavorites } from 'src/app/shared/models/favorite/favorite';
 })
 export class HomeComponent implements OnInit {
 
+  apiKey = environment.collegeAPIkey;
   search_mode = "Recommended";
 
   school_name: String = "";
@@ -30,7 +36,9 @@ export class HomeComponent implements OnInit {
   constructor(
     public authService: AuthService,
     public searchService: SearchService,
-    public favoriteService: FavoritesService) {
+    public favoriteService: FavoritesService,
+    private http: HttpClient,
+    private firestore: AngularFirestore) {
 
   }
 
@@ -43,19 +51,7 @@ export class HomeComponent implements OnInit {
       error => {
         console.log(error);
       });
-    this.searchService.recommended().subscribe(
-      result => {
-        this.schools = result.results;
-        this.schools.sort(function (a, b) {
-          return a['latest.cost.avg_net_price.overall'] - b['latest.cost.avg_net_price.overall'];
-        });
-        this.loading = false;
-      },
-      error => {
-        console.log(error);
-        this.loading = false;
-        // this.notifService.showNotif(error.toString(), 'warning');
-      });
+    this.searchRecommended();
   }
 
   get isCustom(): boolean {
@@ -72,15 +68,39 @@ export class HomeComponent implements OnInit {
 
   changePage(pageNum: number) {
     this.currentPage = pageNum;
-    this.search();
+    if (this.search_mode === 'Recommended') {
+      this.searchRecommended();
+    }
+    else {
+      this.searchCustom();
+    }
   }
 
-  search() {
+  searchRecommended() {
+    this.loading = true;
+    this.recommended(this.currentPage).subscribe(
+      result => {
+        this.numPages = Math.ceil(result.metadata.total / result.metadata.per_page);
+        this.schools = result.results;
+        // this.schools.sort(function (a, b) {
+        //   return a['latest.cost.avg_net_price.overall'] - b['latest.cost.avg_net_price.overall'];
+        // });
+        this.loading = false;
+      },
+      error => {
+        console.log(error);
+        this.loading = false;
+        // this.notifService.showNotif(error.toString(), 'warning');
+      });
+  }
+
+  searchCustom() {
     this.loading = true;
     this.searchService.search(this.school_name, this.school_size, this.school_cost, this.school_state, this.currentPage).subscribe(
       result => {
         this.numPages = Math.ceil(result.metadata.total / result.metadata.per_page);
         this.schools = result.results;
+        console.log
         this.schools.sort(function (a, b) {
           return a['latest.cost.avg_net_price.overall'] - b['latest.cost.avg_net_price.overall'];
         });
@@ -96,7 +116,85 @@ export class HomeComponent implements OnInit {
   searchButton() {
     this.currentPage = 0;
     this.numPages = 0;
-    this.search();
+    this.searchCustom();
   }
+
+  recommended(pageNum: number = 0) {
+    const doc = this.firestore.doc<any>('surveys/' + this.authService.currentUserValue.uid);
+    doc.valueChanges().subscribe( data => {
+      if (data != null) {
+        const formData: SurveyForm = data;
+        console.log(data);
+      }
+    })
+    
+    const params = {
+      "api_key": this.apiKey,
+      "page": pageNum,
+      "sort": "latest.school.name",
+      "school.region.id": [1,2,5,6],
+      "school.locale": [11,12,13,21,22,23,31,32,33],
+      "latest.cost.avg_net_price.overall__range": "..18000",
+      "latest.student.size__range": "15000..",
+      "latest.school.men_only": 0,
+      "latest.school.women_only": 0,
+      "school.ownership": [1,2,3],
+      "school.degrees_awarded.predominant": 3,
+      "latest.admissions.sat_scores.average.overall__range": "..1300"
+    };
+    const url = this.linkFactory(params);
+    console.log(url);
+    return this.http.get<SearchResults>(url);
+    // https://api.data.gov/ed/collegescorecard/v1/schools?api_key=yAZ3JaZzDbjzTLPrdXmazvRcHrHqYDXbbdRFbvka&page=1&sort=latest.school.name&school.region.id=1,2,5&school.locale=11,12,13,21,22,23,31,32,33&latest.cost.avg_net_price.overall__range=..18000&latest.student.size__range=5000..15000&latest.school.men_only=0&latest.school.women_only=0&school.ownership=1,2,3&school.degrees_awarded.predominant=3&latest.admissions.sat_scores.average.overall__range=..1300&fields=id,latest.school.zip,latest.school.city,latest.school.name,latest.school.alias,latest.school.state,latest.school.school_url,latest.student.size,latest.student.demographics.men,latest.student.demographics.women,latest.cost.avg_net_price.overall
+  }
+
+  linkFactory(queryParams: any, inDepth: boolean = false) {
+    var link = `https://api.data.gov/ed/collegescorecard/v1/schools?`;
+    queryParams.fields = inDepth ? SearchService.inDepthFields :  SearchService.fields;
+    link += this.addQueryParams(queryParams);
+    return link;
+  }
+
+  addQueryParams(queryParams: any): string {
+    const keys = []
+    for (const key in queryParams) {
+      keys.push(encodeURIComponent(key) + "=" + queryParams[key]);
+    }
+    return keys.join('&');
+  }
+
+  static fields = [
+    "id",
+    "latest.school.zip",
+    "latest.school.city",
+    "latest.school.name",
+    "latest.school.alias",
+    "latest.school.state",
+    "latest.school.school_url",
+    "latest.student.size",
+    "latest.student.demographics.men",
+    "latest.student.demographics.women",
+    "latest.cost.avg_net_price.overall"
+  ];
+
+  static inDepthFields = [
+    "id",
+    "latest.school.zip",
+    "latest.school.city",
+    "latest.school.name",
+    "latest.school.alias",
+    "latest.school.state",
+    "latest.school.school_url",
+    "latest.student.size",
+    "latest.student.demographics.men",
+    "latest.student.demographics.women",
+    "latest.cost.avg_net_price.overall",
+    "latest.school.price_calculator_url",
+    "latest.school.degree_urbanization",
+    "latest.school.degrees_awarded.predominant",
+    "latest.school.degrees_awarded.highest",
+    "latest.school.ownership",
+    "latest.admissions.admission_rate.overall"
+  ]
 
 }
