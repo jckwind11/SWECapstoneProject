2f4ae21797a97e8b42296c6a4166e21622095af8
Matthew Lyman <mjlyman@comcast.net>
2022-05-02 11:09:57 -0400

    survey work

--- college-finder-app/src/app/components/survey/survey.component.ts
+++ college-finder-app/src/app/components/survey/survey.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, NgForm } from '@angular/forms';
+import { SurveyQuestion, SurveyQuestionHandler, QuestionType } from 'src/app/shared/models/survey/questions';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
@@ -9,23 +10,151 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class SurveyComponent implements OnInit {
 
-  form: FormGroup
+  allQuestions: SurveyQuestion[];
+  QuestionType = QuestionType;
+  errorMessage: string = '';
+  updateSuccess: boolean = false;
+
+  // Question 1: Region
+  question1_0 = '';
+  question1_1 = '';
+  question1_2 = '';
+  question1_3 = '';
+  question1_4 = '';
+  question1_5 = '';
+  question1_6 = '';
+  question1_7 = '';
+  question1_8 = '';
+
+  // Question 2: Urbanization
+  question2_0 = '';
+  question2_1 = '';
+  question2_2 = '';
+  question2_3 = '';
+
+  // Question 3: Size 
+  question3_0 = '';
+  question3_1 = '';
+  question3_2 = '';
+
+  // Question 4: SAT
+  question4 = null;
+
+  // Question 5:  ACT
+  question5 = null;
+
+  // Question 6: State
+  question6 = '';
+
+  // Question 7: Max Cost
+  question7 = 0;
+
+  // Question 8: College type (Co-ed, etc.)
+  question8 = 0;
+
+  // Question 9:  Major
+  question9 = 0;
+
+  // Question 10: Degree
+  question10 = 0;
+
+  // Question 11: Public or private
+  question11 = 0;
 
   constructor(
     public authService: AuthService, 
-    private formBuilder: FormBuilder, 
+    public questionHandler: SurveyQuestionHandler
   ) { 
 
-    this.form = this.formBuilder.group({
-
-    })
+    this.allQuestions = questionHandler.allQuestions;
   }
 
   ngOnInit(): void {
   }
 
-  updateSurvey() {
+  saveSurvey(f: NgForm) {
+    console.log(f.value);
+    if (!this.validate(f)) {
+      console.log('form not valid')
+      return
+    }
 
+    console.log('form IS valid')
+    // Handle form submission
   }
 
+  validate(f: NgForm): boolean {
+    this.errorMessage = '';
+
+    console.log(f.value.question1_0);
+    // Question 1: Make sure at least 1 is checked
+    if (f.value.question1_0 == '' && f.value.question1_1 == '' && f.value.question1_2 == '' && f.value.question1_3 == '' && f.value.question1_4 == '') {
+      if(f.value.question1_5 == '' && f.value.question1_6 == '' && f.value.question1_7 == '' && f.value.question1_8 == '') {
+        this.errorMessage = "Please select at least one region";
+        return false;
+      }
+    }
+
+    // Question 2: Make sure at least one is checked
+    if (f.value.question2_0 == '' && f.value.question2_1 == '' && f.value.question2_2 == '' && f.value.question2_3 == '') {
+      this.errorMessage = "Please select at least one degree of urbanization";
+      return false;
+    }
+
+    // Question 3: Select at least one
+    if (f.value.question3_0 == '' && f.value.question3_1 == '' && f.value.question3_2 == '') {
+      this.errorMessage = "Please select at least one college size";
+      return false;
+    }
+
+    // Question 4: SAT score range 400-1600
+    if (f.value.question4 != '' && f.value.question4 !== null && f.value.question4 < 400 || f.value.question4 > 1600) {
+      this.errorMessage = "Invalid SAT Score";
+      return false;
+    }
+
+    // Question 5: ACT score range 1-36
+    if (f.value.question5 != '' && f.value.question5 !== null && f.value.question5 < 1 || f.value.question5 > 36) {
+      this.errorMessage = "Invalid ACT Score";
+      return false;
+    }
+
+    // Question 6: State
+    if (f.value.question6 == '') {
+      this.errorMessage = "Please select a state";
+      return false;
+    }
+
+    // Question 7: Max Cost
+    if (f.value.question7 == '' || f.value.question7 == null) {
+      this.errorMessage = "Please enter a max cost";
+      return false;
+    }
+
+    // Question 8: College type
+    if (f.value.question8 == '' || f.value.question8 == '0') {
+      this.errorMessage = "Please enter a college type";
+      return false;
+    }
+
+    // Question 9: Major
+    if (f.value.question9 == '' || f.value.question9 == '0') {
+      this.errorMessage = "Please enter a major";
+      return false;
+    }
+
+    // Question 10: degree
+    if (f.value.question10 == '' || f.value.question10 == '0') {
+      this.errorMessage = "Please enter a degree type";
+      return false;
+    }
+
+    // Question 11: Public/Private
+    if (f.value.question11 == '' || f.value.question11 == '0') {
+      this.errorMessage = "Please answer question 11";
+      return false;
+    }
+
+    return true;
+  }
 }
--- college-finder-app/src/app/shared/models/survey/questions.ts
+++ college-finder-app/src/app/shared/models/survey/questions.ts
@@ -1,1 +1,84 @@
-//TODO
+import { Injectable, NgZone } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class SurveyQuestionHandler {
+
+    question1: SurveyQuestion = {
+        question: "What region do you prefer?",
+        answerChoices: ["New England", "Mid East", "Great Lakes", "Plains", "Southeast", "Southwest", "Rocky Mountains", "Far West", "Outlying Areas (AS, FM, GU, MH, MP, PR, PW, VI)"],
+        questionType: QuestionType.Checkbox
+    }
+
+    question2: SurveyQuestion = {
+        question: "What degrees of urbanization would you like the college to be in?",
+        answerChoices: ["City", "Suburb", "Town", "Rural"],
+        questionType: QuestionType.Checkbox
+    }
+
+    question3: SurveyQuestion = {
+        question: "What size college are you interested in?",
+        answerChoices: ["Small (Less than 5,000)", "Medium (Between 5,000 and 15,000)", "Large (Greater than 15,000)"],
+        questionType: QuestionType.Checkbox
+    }
+    
+    question4: SurveyQuestion = {
+        question: "What was your SAT score? (Leave blank if you don't have one)",
+        questionType: QuestionType.Number
+    }
+
+    question5: SurveyQuestion = {
+        question: "What was your ACT score? (Leave blank if you don't have one)",
+        questionType: QuestionType.Number
+    }
+
+    question6: SurveyQuestion = {
+        question: "What state are you located in? (Used for in-state tuition calculation)",
+        questionType: QuestionType.Dropdown
+    }
+
+    question7: SurveyQuestion = {
+        question: "What is the maximum cost you are you willing to pay per year? (In USD)",
+        questionType: QuestionType.Number
+    }
+
+    question8: SurveyQuestion = {
+        question: "What type of college are you looking for?",
+        answerChoices: ["", "Co-ed", "Men Only", "Women Only"],
+        questionType: QuestionType.Dropdown
+    }
+
+    question9: SurveyQuestion = {
+        question: "What major are you interested in?",
+        answerChoices: ["", "Other", "Engineering", "Education", "Legal", "Business_marketing", "Social Science", "Physical Science", "Agriculture", "Architecture"],
+        questionType: QuestionType.Dropdown
+    }
+
+    question10: SurveyQuestion = {
+        question: "What type of degree are you looking for?",
+        answerChoices: ["", "Non-degree Granting", "Certificate Degree", "Associate Degree", "Bachelor's Degree", "Graduate Degree"],
+        questionType: QuestionType.Dropdown
+    }
+
+    question11: SurveyQuestion = {
+        question: "Would you like a Public or Private college?",
+        answerChoices: ["", "Public", "Private", "Either"],
+        questionType: QuestionType.Dropdown
+    }
+
+    allQuestions: SurveyQuestion[] = [this.question1, this.question2, this.question3, this.question4, this.question5, this.question6, this.question7, this.question8, this.question9, this.question10, this.question11];
+
+}
+
+export interface SurveyQuestion {
+    question: String
+    answerChoices?: String[];
+    questionType: QuestionType
+}
+
+export enum QuestionType {
+    Dropdown,
+    Checkbox,
+    Number
+}
